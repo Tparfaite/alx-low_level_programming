610a81645d2b64696c8b55eba6dbfd840de47228
Tparfaite <parfaitetwagira@gmail.com>
2022-10-13 12:39:36 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 {
-	printf("and that piece of art is useful - Dora Korpar, 2015-10-19\n");
+	printf("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
 	return(1);
 }
