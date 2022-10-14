e127bca40f97ff890c8068ef15de807cf825c790
Tparfaite <parfaitetwagira@gmail.com>
2022-10-14 13:18:54 -0700

    digits for base ten

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ * prints all single digit of base ten
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d\n", a);
+	}
+	return (0);
+}
