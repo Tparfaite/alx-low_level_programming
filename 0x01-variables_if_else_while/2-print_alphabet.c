15da3bdf9925dcf17eb09bac05041ec92a439ebd
Tparfaite <parfaitetwagira@gmail.com>
2022-10-14 12:02:29 -0700

    change

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,13 +6,13 @@
  *
  * Return: Always 0 (success)
  */
- int main(void)
+  int main(void)
 {
 	char c;
-	for(c='a'; c<='z'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 	{
 		putchar (c);
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
