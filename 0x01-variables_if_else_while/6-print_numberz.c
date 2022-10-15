8e4ab4981553ca43e0335c57e54818b269380d2b
Tparfaite <parfaitetwagira@gmail.com>
2022-10-15 11:32:43 -0700

    nums

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 /**
- * main - Prints single digits with putchar
+ * main -Entry point
+ *
+ * Prints single digits with putchar
  *
  * Return: always 0 (success)
  */
@@ -8,17 +10,11 @@ int main(void)
 {
 	int n;
 
-<<<<<<< HEAD
-	for (n = 0; n < 10; n++)
-		putchar(n);
-	putchar('\n');
-=======
+
 	for (n = 0 ; n <= 9 ; n++)
 		putchar(n + '0');
 
 	putchar('\n');
-
->>>>>>> ed0811e188f5acce71ca264ca9ebaed9c73b0f47
 	return (0);
 }
 
