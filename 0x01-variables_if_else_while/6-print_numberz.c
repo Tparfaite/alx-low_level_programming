9b6a769fca6b60850d43ebadd083eb88727b6aa0
Tparfaite <105437186+Tparfaite@users.noreply.github.com>
2022-10-15 21:28:56 +0200

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 /**
- * main -Entry point
+ * main - Prints single digits with putchar
  *
- * Prints single digits with putchar
- *
- * Return: always 0 (success)
+ * Return: always 0.
  */
 int main(void)
 {
 	int n;
 
-
 	for (n = 0 ; n <= 9 ; n++)
 		putchar(n + '0');
 
 	putchar('\n');
+
 	return (0);
 }
 
