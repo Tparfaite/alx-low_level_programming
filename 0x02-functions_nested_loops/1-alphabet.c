63f7de1b39d8fa39d8eb315a3a7aa8bd07e95cae
Tparfaite <parfaitetwagira@gmail.com>
2022-10-18 08:32:51 -0700

    putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ *
+ * print alphabets in lowercase
+ */
+void print_alphabet(void)
+{
+	char m;
+
+	for (m = 'a'; m < 'z'; m++)
+	{
+		_putchar(m);
+	}
+	_putchar('\n');
+}
