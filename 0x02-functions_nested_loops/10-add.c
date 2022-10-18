b929767cec86f3f5954785b9fc967f6e6b0c5360
Tparfaite <parfaitetwagira@gmail.com>
2022-10-18 13:39:35 -0700

    sum

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - Adds two integers
+ *
+ * @x: number one
+ * @y: number two
+ *
+ * Return: sum of x and y
+ */
+int add(int x, int y)
+{
+	int sum = x + y;
+
+	return (sum);
+}
