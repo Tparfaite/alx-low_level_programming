fb2ca66b4e86b00c6e3be033917090a7cd9b22a8
Tparfaite <parfaitetwagira@gmail.com>
2022-10-18 08:45:12 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,13 @@
-#include "main.h"
 /**
  * main - Entry point
  *
  * Return: Always 0 (success)
  *
- *Description:  print alphabets in lowercase
+ * Description:  print alphabets in lowercase
+ */
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line
  */
 void print_alphabet(void)
 {
