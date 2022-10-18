9f2ea4df1825c2b1c2c468700f8f0ad77253b2d2
Tparfaite <parfaitetwagira@gmail.com>
2022-10-18 12:04:27 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - function that computes the absolute value of ineger
+ *
+ * @n: integer to be computed
+ *
+ * Return: Absolute value
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = (-1) * n;
+		return (n);
+	}
+}
+
