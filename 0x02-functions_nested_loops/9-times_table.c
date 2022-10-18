6645d7582d5f4342f84ad5ddcef41942647de23f
Tparfaite <parfaitetwagira@gmail.com>
2022-10-18 13:31:14 -0700

    table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,7 +8,7 @@ void times_table(void)
 
 	for (num = 0; num <= 9; ++num)
 	{
-		_putchar('0');
+		_putchar(48);
 		for (mult = 1; mult <= 9; ++mult)
 		{
 			_putchar(',');
@@ -21,11 +21,11 @@ void times_table(void)
 			}
 			else
 			{
-				_putchar((prod / 10) + '0');
-				_putchar((prod % 10) + '0');
+				_putchar((prod / 10) + 48);
 			}
-			_putchar('\n');
+			_putchar((prod % 10) + 48);
 		}
+		_putchar('\n');
 	}
 
 }
