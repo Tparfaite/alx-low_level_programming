e00fe5ac9c908bc264836a3001c0d3a5d63fff3c
Tparfaite <parfaitetwagira@gmail.com>
2022-10-20 05:53:41 -0700

    most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,9 +7,12 @@ void print_most_numbers(void)
 {
 	int a;
 
-	if (a != 2 && a !=4)
+	for (a = 0; a <= 9; a++)
 	{
-		_putchar((a % 10) + '0');
+		if (a !=2 && a != 4)
+		{
+			_putchar((a % 10) + '0');
+		}
 	}
 	_putchar('\n');
 }
